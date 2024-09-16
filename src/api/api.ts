b78ee417e7394887d2d681aac48dd28797940d18
nym-tmn <nymtmn8690@gmail.com>
2024-09-16 23:26:50 +0500

    Added workflow

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,5 @@
 const baseUrl = "https://api.github.com/graphql";
 
-
 /* Функция для извлечения списка репозиториев из GitHub, соответствующих заданному значению поиска (searchValue).
  Поддерживает пагинацию с использованием параметров `endCursorValue` и `startCursorValue`, а также задает размер страницы с использованием параметра `pageSize` */
 export const getRepositories = async (searchValue: string, endCursorValue: string = '', startCursorValue: string = '', pageSize: number = 10): Promise<Response> => {
@@ -41,7 +40,8 @@ export const getRepositories = async (searchValue: string, endCursorValue: strin
 	const options = {
 		method: 'POST',
 		headers: {
-			"Authorization": "bearer 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",
+			// "Authorization": "bearer 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",
+			"Authorization": `bearer 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`,
 		},
 		body: JSON.stringify({ query })
 	};
